7900363336668998407b388b584bedc726e2b204
Vaisakh <vaisakhpn78@gmail.com>
2024-02-24 22:15:47 +0530

    Fetched Google api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,9 @@
-export default function WebSearchPage() {
-  return (
-    <div>WebSearchPage</div>
-  )
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const result = data.items;
+
+  return <div>{result && result.map((result) => <h1>{result.title}</h1>)}</div>;
 }
